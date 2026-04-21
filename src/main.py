1299de376d994ec9a212354df2c94de172de7827
Millenmark <millenmark.dev@gmail.com>
2026-04-21 09:46:31 +0800

    parameters

--- src/main.py
+++ src/main.py
@@ -6,3 +6,13 @@ app = FastAPI(docs_url=None, redoc_url=None)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/parameter/{sample}")
+async def parameter(sample: str):
+    return {"sample": f"this is the parameter: {sample}"}
+
+
+@app.get("/query-parameter")
+async def query_parameter(name: str) -> dict:
+    return {"name": f"this is the query parameter: {name}"}
