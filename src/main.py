17d2243ceaed72bb9ea5b930b4a6a7b8ac08ac8d
Millenmark <millenmark.dev@gmail.com>
2026-04-21 13:15:47 +0800

    payload schema

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI(docs_url=None, redoc_url=None)
 
@@ -18,3 +19,13 @@ async def parameter(sample: str):
 @app.get("/query-parameter")
 async def query_parameter(name: str = "Username", age: Optional[int] = 0) -> dict:
     return {"name": f"this is the query parameter: {name}", "age": age}
+
+
+class Book(BaseModel):
+    title: str
+    author: str
+
+
+@app.post("/book")
+async def create_book(book: Book) -> dict:
+    return {"title": book.title, "author": book.author}
